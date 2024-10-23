10c95a88ae521803310e5e9e33a59b57d701eaef
sathish <me.sathish.git@gmail.com>
2024-10-23 08:32:53 +0530

    update design changes

--- src/app/home/components/projects/projects.component.ts
+++ src/app/home/components/projects/projects.component.ts
@@ -13,43 +13,37 @@ export class ProjectsComponent {
       name: 'Shuriken',
       image: 'projects/shuriken.png',
       url: 'https://shuriken-omega.vercel.app/',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid natus rem ut optio sed at, a laudantium vero quidem alias!',
+      description: `This app provides an engaging platform for fans of the Naruto series to explore characters and their details.`,
     },
     {
       name: 'Pick Me A Theme',
       image: 'projects/pickmeatheme.png',
       url: 'https://pickmeatheme.xyz/',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid natus rem ut optio sed at, a laudantium vero quidem alias!',
+      description: `"Pick Me A Theme" is an innovative application designed to help users create personalized color themes for various projects`,
     },
     {
       name: 'Todo Firebase',
       image: 'projects/todo-firebase.png',
       url: 'https://todo-firebase-123.web.app/',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid natus rem ut optio sed at, a laudantium vero quidem alias!',
+      description: `Todo Application is a task management tool built using Firebase for backend services.`,
     },
     {
       name: 'Portfolio 2',
       image: 'projects/sathish-code.png',
       url: 'https://sathish-code.web.app/',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid natus rem ut optio sed at, a laudantium vero quidem alias!',
+      description: `This portfolio website is a personal showcase designed to highlight an individual's skills, projects, and professional experiences. It serves as an online resume and creative outlet.`,
     },
     {
       name: 'Random Dogs',
       image: 'projects/randomova.png',
       url: 'https://randomova.vercel.app/dogs/v1',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid natus rem ut optio sed at, a laudantium vero quidem alias!',
+      description: `This application designed to calm users, the core functionality would likely revolve around features aimed at reducing stress and anxiety.`,
     },
     {
       name: 'Book store',
       image: 'projects/book-store.png',
       url: 'https://showcase-apps-plum.vercel.app/',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid natus rem ut optio sed at, a laudantium vero quidem alias!',
+      description: `Bookstore app is designed to provide users with a convenient and engaging platform for discovering, purchasing, and enjoying books.`,
     },
   ];
 }
